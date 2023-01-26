7f7121566b9b395740b93db0ff0441d32fa3bfc9
alex.xu <alex.xu84@yahoo.com>
2023-01-26 16:02:21 -0600

    QA component folder have been updated

---
+++ client/src/components/QAcomponents/ListEntry.jsx
@@ -0,0 +1,14 @@
+import Qbody from './Qbody.jsx';
+import AList from './AList.jsx';
+
+const ListEntry = () => {
+
+  return (
+    <div>
+    <Qbody />
+    <AList />
+    </div>
+  )
+}
+
+export default ListEntry
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import { render } from "react-dom";
-
+import QAIndex from './components/QAcomponents/QAIndex.jsx';
 
 const App = () => {
   return (
     <div>
       <h1>Hello Iron Man!</h1>
+      <QAIndex />
+
     </div>
   )
 }
