46fa3daa35410702ce8f99a562601619f85c07e6
Agnese <117535620+Agni-Cast@users.noreply.github.com>
2023-01-26 16:35:19 -0500

    Refactor src/index.js to use ReactDOM to render page; import RatingsReviews.jsx to src/index.js Create a RatingsReviews parent component and two subcomponents Ratings, Reviews

---
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import Ratings from './Ratings.jsx';
+import Reviews from './Reviews.jsx';
+
+
+const RatingsReviews = () => {
+  return (
+    <div>
+      <h5> RATINGS & REVIEWS</h5>
+      <Ratings/>
+      <Reviews/>
+    </div>
+  );
+}
+
+export default RatingsReviews;
