34f57cb3ff3f32584f714f21ad3d2961fdb05812
Paul Koski <pwkoski@gmail.com>
2023-01-26 17:25:17 -0500

    add the get/products route in server/index.js

--- Server/index.js
+++ Server/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
 const app = express();
+const axios = require('axios');
+const {TOKEN} = require('../config.js');
 
 const PORT = process.env.PORT || 3000;
 
+//Middleware:
+
 app.use(express.json());
 
+
+//Overview routes:
+
+//TODO: fix function so that page and count parameters go through to axios request.
+app.get('/products', (req, res) => {
+  axios.get('https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/products',
+  {
+   headers: {
+    'Authorization': `${TOKEN}`
+  }
+  })
+  .then((response) => {
+    console.log('this is product list', response.data);
+    res.status(200).send(response.data); //.send(productList);
+  })
+  .catch((error) => {
+    console.log(error);
+    res.status(501).send(error);
+  });
+});
 
 
 // Update
