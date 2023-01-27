2a8f250608afac5a2e7e7fffcd3d2633395c1a29
Agnese <117535620+Agni-Cast@users.noreply.github.com>
2023-01-27 11:10:05 -0500

    api connection

--- Server/index.js
+++ Server/index.js
@@ -1,4 +1,6 @@
 const path = require("path");
+const config =require('../config.js')
+const axios = require('axios');
 
 const express = require('express');
 const app = express();
@@ -9,5 +11,22 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/dist")))
 
 
-// Update
+
+// app.get('/reviews/', function(req, res) {
+//   return axios({
+//     method: req.method,
+//     url: `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rfe/`,
+//     headers: {
+//       'User-Agent': 'request',
+//       'Authorization': `token ${config.TOKEN}`
+//     },
+//     data: req.body
+//   })
+//   .then((response) => {
+//     console.log(response)
+//   res.send(response.data)
+//   })
+// })
+
+
 app.listen(PORT, () => console.log(`listening on port ${PORT}`));
