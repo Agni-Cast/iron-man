0f95ded4a4303645a3787a5ff4f1e210df554b27
alex.xu <alex.xu84@yahoo.com>
2023-01-27 13:24:44 -0600

    now can read from API, specific question helpfulness number and accumulate and show on the page

--- client/src/components/QAcomponents/ListEntry.jsx
+++ client/src/components/QAcomponents/ListEntry.jsx
@@ -2,22 +2,20 @@ import AList from './AList.jsx';
 import {useState} from 'react';
 
 const ListEntry = (props) => {
-  // console.log("props i gave to Alist are :", {props});
-  // set state for just render two answers at begging
-  // const [numAnswerToShow, setnumAnswerToShow] = useState(2);
+  // set helpfulnessCount for showing the current question helpfulness data
+  const [helpfulnessCount, setHelpfulnessCount] = useState(props.question.question_helpfulness)
 
-  // const loadMoreAnswers = () => {
-  //   setnumAnswerToShow(numAnswerToShow + 2);
-  // }
+  const voteOnHelp = (questionId) => {
+    setHelpfulnessCount(helpfulnessCount + 1);
+  }
 
-  // const answers = props.question.answers;
-  // console.log(Object.values(answers));
+
   return (
     <div>
       <div>
         <h2>Q: {props.question.question_body}</h2>
         <p>
-          <button onClick={() => voteOnHelp(props.question.quesiton_id)}>Helpful? <a url="rul">YES</a></button>
+          <button onClick={() => voteOnHelp(props.question.quesiton_id)}>Helpful? <a url="rul">YES</a> <a>{helpfulnessCount}</a></button>
           <button onClick={() => addAnswer(props.question.question_id)}>Add Answer</button>
         </p>
       </div>
