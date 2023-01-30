f934c218b1c90f822d76dda5d93e917fbeb44f3c
Paul Koski <pwkoski@gmail.com>
2023-01-30 11:32:22 -0500

    add main photo, add product_id prop to index.jsx

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,12 +1,21 @@
 import React from "react";
+import {useEffect, useState} from "react";
 import QAIndex from './components/QAcomponents/QAIndex.jsx';
 import ReactDOM from "react-dom";
-import RatingsReviews from "./components/RatingsReviews/RatingsReviews.jsx"
+import RatingsReviews from "./components/RatingsReviews/RatingsReviews.jsx";
+import Overview from "./components/OverviewComponents/Overview.jsx";
+
 
 const App = () => {
+
+  const [productID, setProductID] = useState(37311);
+
+
+
   return (
     <div>
       <h1>Hello Iron Man!</h1>
+      <Overview productID={productID}/>
       <QAIndex />
       <RatingsReviews/>
     </div>
